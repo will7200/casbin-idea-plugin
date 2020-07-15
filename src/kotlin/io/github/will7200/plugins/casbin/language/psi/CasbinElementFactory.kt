af645903f8fcefc6b0e98855c9191861cd01af74
William Flores <will7207@gmail.com>
2020-07-15 16:05:17 -0700

    fix: createFunctionName didn't produce correct psi elements

--- src/kotlin/io/github/will7200/plugins/casbin/language/psi/CasbinElementFactory.kt
+++ src/kotlin/io/github/will7200/plugins/casbin/language/psi/CasbinElementFactory.kt
@@ -1,14 +1,27 @@
 package io.github.will7200.plugins.casbin.language.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.util.findDescendantOfType
 import io.github.will7200.plugins.casbin.language.CasbinFileType
 
 object CasbinElementFactory {
+    private val log: Logger = Logger.getInstance(CasbinElementFactory::class.java)
+
     fun createFile(project: Project, text: String) =
-        PsiFileFactory.getInstance(project).createFileFromText("Makefile", CasbinFileType, text) as CasbinPsiFile
+        PsiFileFactory.getInstance(project).createFileFromText("policy.conf", CasbinFileType, text) as CasbinPsiFile
 
     fun createSectionName(project: Project, section: String) =
         createFile(project, section).firstChild as ASTNode
+
+    fun createFunctionName(project: Project, functionName: String): CasbinFunctionName {
+        return createFile(
+            project, """
+        [fake_section]
+        e = ${functionName}(fakeargs)
+    """.trimIndent()
+        ).findDescendantOfType { true }!!
+    }
 }
