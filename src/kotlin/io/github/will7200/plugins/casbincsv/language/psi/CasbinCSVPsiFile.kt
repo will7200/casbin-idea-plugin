606379118db162740fe178523c71264d78c50130
William Flores <will7207@gmail.com>
2020-07-21 22:20:09 -0700

    feat: add record collection for CasbinCSVPsiFile

--- src/kotlin/io/github/will7200/plugins/casbincsv/language/psi/CasbinCSVPsiFile.kt
+++ src/kotlin/io/github/will7200/plugins/casbincsv/language/psi/CasbinCSVPsiFile.kt
@@ -2,6 +2,7 @@ package io.github.will7200.plugins.casbincsv.language.psi
 
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.util.PsiTreeUtil
 import io.github.will7200.plugins.casbincsv.language.CasbinCSVFileConstants
 import io.github.will7200.plugins.casbincsv.language.CasbinCSVFileType
 import io.github.will7200.plugins.casbincsv.language.CasbinCSVLanguage
@@ -12,4 +13,6 @@ class CasbinCSVPsiFile(viewProvider: FileViewProvider) : PsiFileBase(
 ) {
     override fun getFileType() = CasbinCSVFileType
     override fun toString() = CasbinCSVFileConstants.PSI_FILE_NAME
+    val records: Collection<CasbinCSVRecord>
+        get() = PsiTreeUtil.findChildrenOfType(this, CasbinCSVRecord::class.java)
 }
