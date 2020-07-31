918eced731e57bae81bf34685b2a2698e71f959b
William Flores <will7207@gmail.com>
2020-07-31 14:15:38 -0700

    feat: implement async button for tool window

--- src/kotlin/io/github/will7200/plugins/casbin/view/ui/CasbinToolWindowFactory.kt
+++ src/kotlin/io/github/will7200/plugins/casbin/view/ui/CasbinToolWindowFactory.kt
@@ -1,10 +1,12 @@
 package io.github.will7200.plugins.casbin.view.ui
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.Content
@@ -13,12 +15,15 @@ import io.github.will7200.plugins.casbin.CasbinDocumentRequest
 import io.github.will7200.plugins.casbin.CasbinDocumentService
 import io.github.will7200.plugins.casbin.CasbinTopics
 import io.github.will7200.plugins.casbin.view.editors.CasbinCSVEditor
+import java.awt.event.ItemEvent
 
 
 class CasbinToolWindowFactory : ToolWindowFactory {
+    private lateinit var casbinDocumentManager: CasbinDocumentService
     private val log: Logger = Logger.getInstance(CasbinToolWindowFactory::class.java)
     private lateinit var project: Project
     private lateinit var myToolWindow: CasbinExecutorToolWindow
+    private var dispose: Disposable? = null
 
     // Create the tool window content.
     override fun createToolWindowContent(
@@ -30,13 +35,41 @@ class CasbinToolWindowFactory : ToolWindowFactory {
         val contentFactory = ContentFactory.SERVICE.getInstance()
         val content: Content = contentFactory.createContent(myToolWindow.content, "Main", false)
         toolWindow.contentManager.addContent(content)
+        casbinDocumentManager = project.service()
         setupListeners()
     }
 
     private fun setupListeners() {
-        val casbinDocumentManager = project.service<CasbinDocumentService>()
         myToolWindow.requestEditorPane.run {
             casbinDocumentManager.connectDocument(document)
+        }
+
+        if (myToolWindow.runAsyncCheckBox.isSelected) {
+            connectDocumentAsync()
+        }
+        myToolWindow.runTestButton.addActionListener {
+            project.messageBus.syncPublisher(CasbinTopics.DOCUMENT_REQUEST_TOPIC)
+                .processChange(
+                    CasbinDocumentRequest.ExecuteEntireDocument(
+                        myToolWindow.requestEditorPane as CasbinCSVEditor,
+                        myToolWindow.requestEditorPane.document,
+                        myToolWindow
+                    )
+                )
+        }
+
+        myToolWindow.runAsyncCheckBox.addItemListener { event ->
+            if (event.stateChange == ItemEvent.SELECTED) {
+                connectDocumentAsync()
+            } else {
+                disconnectDocument()
+            }
+        }
+    }
+
+    private fun connectDocumentAsync() {
+        dispose = Disposable {}
+        myToolWindow.requestEditorPane.run {
             document.addDocumentListener(object : DocumentListener {
                 override fun documentChanged(event: DocumentEvent) {
                     super.documentChanged(event)
@@ -59,22 +92,15 @@ class CasbinToolWindowFactory : ToolWindowFactory {
                         )
                     }
                 }
-            })
+            }, dispose!!)
         }
-        myToolWindow.runTestButton.addActionListener { event ->
-            project.messageBus.syncPublisher(CasbinTopics.DOCUMENT_REQUEST_TOPIC)
-                .processChange(
-                    CasbinDocumentRequest.ExecuteEntireDocument(
-                        myToolWindow.requestEditorPane as CasbinCSVEditor,
-                        myToolWindow.requestEditorPane.document,
-                        myToolWindow
-                    )
-                )
-        }
+    }
 
-        // TODO: Async Check Box
-        myToolWindow.runAsyncCheckBox.addActionListener { event ->
-            log.warn(event.paramString())
+    private fun disconnectDocument() {
+        myToolWindow.requestEditorPane.run {
+            dispose?.let {
+                Disposer.dispose(it)
+            }
         }
     }
 }
