aafb7d0d81cdfabe81ae05b35593c41c4a6bb235
William Flores <will7207@gmail.com>
2020-12-15 15:19:14 -0800

    fix: remove deprecated psitreechange listener

--- src/kotlin/io/github/will7200/plugins/casbin/language/structure/CasbinStructureViewFactory.kt
+++ src/kotlin/io/github/will7200/plugins/casbin/language/structure/CasbinStructureViewFactory.kt
@@ -5,17 +5,11 @@ import com.intellij.ide.structureView.TreeBasedStructureViewBuilder
 import com.intellij.lang.PsiStructureViewFactory
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiManager
-import io.github.will7200.plugins.casbin.language.psi.StructurePsiChangeListener
 
 class CasbinStructureViewFactory : PsiStructureViewFactory {
     override fun getStructureViewBuilder(file: PsiFile) = object : TreeBasedStructureViewBuilder() {
 
         override fun createStructureViewModel(editor: Editor?): StructureViewModel {
-            val project = editor?.project ?: return CasbinStructureViewModel(file, editor)
-            val manager = PsiManager.getInstance(project)
-            manager.addPsiTreeChangeListener(StructurePsiChangeListener(project))
-
             return CasbinStructureViewModel(file, editor)
         }
     }
