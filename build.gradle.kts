3285586504430254c079022fbaf829d62367b6d5
William Flores <will7207@gmail.com>
2023-12-07 09:07:41 -0800

    chore: update intellij plugin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.0"
     // gradle-intellij-plugin - read mo
     // re: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.8.1"
+    id("org.jetbrains.intellij") version "1.10.2"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.3.1"
 }
